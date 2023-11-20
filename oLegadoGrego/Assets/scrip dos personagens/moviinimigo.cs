04c7330f8d90f9c963c08b24252c645d779f978c
LutandoparaAprender <mateusdejesus304@gmail.com>
2023-11-20 20:07:20 -0300

    arrumando scripy
    scripts ajeitados

--- oLegadoGrego/Assets/scrip dos personagens/moviinimigo.cs
+++ oLegadoGrego/Assets/scrip dos personagens/moviinimigo.cs
@@ -2,46 +2,43 @@ using UnityEngine;
 
 public class moviinimigo : MonoBehaviour
 {
-    public Transform target;         // Referência ao jogador (arraste o GameObject do jogador aqui)
-    public float moveSpeed = 3.0f;   // Velocidade de movimento do inimigo
-    public float maxChaseDistance = 10.0f; // Distância máxima para perseguir o jogador
-    public float minChaseDistance = 2.0f; // Distância minima para perseguir o jogador
+    public Transform target;
+    public float moveSpeed = 3.0f;
+    public float maxChaseDistance = 10.0f;
+    public float minChaseDistance = 2.0f;
 
     private Animator animator;
     private SpriteRenderer spriteRenderer;
+
     private void Start()
     {
         animator = GetComponent<Animator>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent < SpriteRenderer>();
     }
+
     private void Update()
     {
-
         if (target != null)
         {
-            // Calcula a distância entre o inimigo e o jogador
             float distanceToTarget = Vector3.Distance(transform.position, target.position);
 
-            // Verifica se o jogador está dentro da distância máxima de perseguição
-            if (distanceToTarget <= maxChaseDistance && distanceToTarget >=minChaseDistance)
+            if (distanceToTarget <= maxChaseDistance && distanceToTarget >= minChaseDistance)
             {
                 Vector3 direction = target.position - transform.position;
                 direction.Normalize();
-
-                // Mova o inimigo na direção do jogador
                 transform.position += direction * moveSpeed * Time.deltaTime;
 
-                animator.SetBool("animacao walk", distanceToTarget <= maxChaseDistance);
+                animator.SetBool("walk ini", true);
 
-                // Flip o sprite se necessário
-                if (direction.x < 0) // Verifica se está se movendo para a esquerda
+                if (direction.x < 0)
                 {
-                    spriteRenderer.flipX = true; // Flip horizontalmente
+                    spriteRenderer.flipX = true;
                 }
                 else
                 {
-                    spriteRenderer.flipX = false; // Não flip
+                    spriteRenderer.flipX = false;
                 }
+                animator.SetBool("walk ini", distanceToTarget > 2.2f);
             }
         }
     }
